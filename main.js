f0d373a1dba8f5519b68090a2b6dced292b14542
Hip Chip <hchip6207@gmail.com>
2026-02-03 18:35:42 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
 const sparky = document.createElement("img");
   sparky.src = "https://media.tenor.com/NYzVC0OHCbwAAAAj/pug-dog-dancing.gif";
-  sparky.width = 25;
-  sparky.height = 50;
+  sparky.width = 500;
+  sparky.height = 500;
   sparky.id = "sparky";
-  sparky.style.bottom = 200;
-  sparky.style.left = 0;
-var x = sparky.style.left;
-var y = sparky.style.bottom;
+  sparky.style.bottom = 0 + "px";
+  sparky.style.left = 0 + "px";
+  sparky.style.position = "fixed";
+var x = 0;
+var y = 0;
 document.addEventListener("keydown", function(event) {
   if (event.key === "ArrowRight") {
-    x = x + 1;
+    x = x + 50;
+    sparky.style.left = x + "px";
   } else if (event.key === "ArrowLeft") {
-    x = x - 1;
+    x = x - 50;
+    sparky.style.left = x + "px";
+  } else if (event.key === "ArrowUp") {
+    y = y + 50;
+    sparky.style.bottom = y + "px";
+  } else if (event.key === "ArrowDown") {
+    y = y - 50;
+    sparky.style.bottom = y + "px";
   }
 });
 
 const STYLE = document.createElement("STYLE");
   STYLE.innerHTML = `
-  #sparky {
-  position: fixed;
+  body {
+  background-image: url('https://gifdb.com/images/high/dancing-pug-dog-with-music-disc-jfg9p16pxr0gohfu.gif');
   }
   `;
 
